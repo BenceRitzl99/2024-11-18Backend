b31286cd782a3e8ba63faabf89c097fc58d23f7f
Ritzl <benceritzl@gmail.com>
2024-11-18 12:30:14 +0100

    óra vége

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { afterRender, Component, Input, OnInit } from '@angular/core';
 import { ScrollDetail } from '@ionic/core';
 
 @Component({
@@ -7,21 +7,61 @@ import { ScrollDetail } from '@ionic/core';
   styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent  implements OnInit {
+  @Input() messages:any
+  scrollTop = 0;
+  scroll = true;
   
+  
+  constructor() {
+    afterRender(
+      ()=>{
+        if (this.scroll){
+          document.getElementsByClassName("vege")[0].scrollIntoView({behavior:"smooth", block:"end", inline:"nearest"});
+        }
+      }
+    )
+   }
+
+
+  
+    ngOnInit() {}
     handleScrollStart() {
       console.log('scroll start');
     }
   
     handleScroll(ev: CustomEvent<ScrollDetail>) {
       console.log('scroll', JSON.stringify(ev.detail));
+      this.scrollTop = Number(ev.detail.scrollTop) 
     }
   
     handleScrollEnd() {
-      console.log('scroll end');
-    }
-  @Input() messages:any
-  constructor() { }
+      let cHeight = document.getElementsByClassName('ion-padding')[0].clientHeight;
+      // console.log("Client Height (ion-padding):" , cHeight);
+      // let sHeight = document.getElementsByClassName('ion-content')[0].scrollHeight;
+      // console.log("Scroll Height (ion-content):" , sHeight);
+      // let conHeight = document.getElementsByClassName('ion-content')[0].clientHeight;
+      // console.log("Content Height (container):" , conHeight);
+      let sonHeight = document.getElementsByClassName('ion-content')[0].scrollTop;
+      
 
-  ngOnInit() {}
+
+      
+      console.log("Scroll Height (container):" , sonHeight);
+      console.log('scroll end');
+    
+    console.log("scrollEnd:", this.scrollTop+cHeight)
+    console.log("scrollEnd:", sonHeight)
+
+    if (this.scrollTop+cHeight>=sonHeight) this.scroll = true
+    else {
+      console.log("Kikapcs")  
+      this.scroll = false
+    }
+    
+}
+  
+  
+
+  
 
 }
